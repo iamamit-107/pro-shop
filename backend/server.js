4ed810f5892b432615eb826ad3dc5c072d90ae00
iamamit-107 <ahmed.amit107@gmil.com>
2020-10-29 00:07:14 +0600

    mongoDB setup and data created

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const products = require("./data/products");
+const connectDB = require("./config/db");
+const productRoute = require("./routes/productRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -10,13 +14,11 @@ app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-    res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use("/api/products", productRoute);
+
+// Custom error handler middleware
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
